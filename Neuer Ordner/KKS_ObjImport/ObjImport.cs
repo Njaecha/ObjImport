ce76a94fbae3eb37aed90f06bb39d60e58b116d1
NiggoJaecha <andre.grabowich@gmail.com>
2021-10-17 03:33:11 +0200

    Initial Upload
    put the source on github they said

---
+++ Neuer Ordner/KKS_ObjImport/ObjImport.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+using KKAPI;
+using KKAPI.Studio.SaveLoad;
+using Studio;
+
+namespace ObjImport
+{
+    [BepInPlugin(GUID, PluginName, Version)]
+    [BepInDependency(KoikatuAPI.GUID, KoikatuAPI.VersionConst)]
+    [BepInDependency(ExtensibleSaveFormat.ExtendedSave.GUID, ExtensibleSaveFormat.ExtendedSave.Version)]
+    [BepInProcess("CharaStudio")]
+    public class ObjImport : BaseUnityPlugin
+    {
+        public string path = "";
+
+        public const string PluginName = "KKS_ObjImport";
+        public const string GUID = "org.njaecha.plugins.objimport";
+        public const string Version = "1.0.0";
+
+        internal new static ManualLogSource Logger;
+
+        private bool uiActive = false;
+        private ConfigEntry<KeyboardShortcut> hotkey;
+        private Rect windowRect = new Rect(500, 40, 200, 80);
+
+        public static List<ObjectCtrlInfo> remeshedObjects = new List<ObjectCtrlInfo>();
+
+        void Awake()
+        {
+            ObjImport.Logger = base.Logger;
+            KeyboardShortcut defaultShortcut = new KeyboardShortcut(KeyCode.O);
+            hotkey = Config.Bind("General", "Hotkey", defaultShortcut, "Press this key to open UI");
+            StudioSaveLoadApi.RegisterExtraBehaviour<SceneController>(GUID);
+        }
+
+        void Update()
+        {
+            if (hotkey.Value.IsDown())
+                uiActive = !uiActive;
+        }
+        public void LoadMesh()
+        {
+            if (!File.Exists(path))
+            {
+                Logger.LogWarning($"File [{path}] does not exist");
+                return;
+            }
+            else
+            {
+                IEnumerable<ObjectCtrlInfo> selectedObjects = KKAPI.Studio.StudioAPI.GetSelectedObjects();
+                Mesh mesh = new Mesh();
+                Boolean runImport = false;
+                List<ObjectCtrlInfo> selectItems = new List<ObjectCtrlInfo>();
+
+                foreach (ObjectCtrlInfo oci in selectedObjects)
+                {
+                    if (oci is OCIItem)
+                    {
+                        OCIItem item = (OCIItem)oci;
+                        if (item.objectItem.GetComponentInChildren<MeshFilter>())
+                        {                            
+                            runImport = true;
+                            selectItems.Add(oci);
+                        }
+                        else Logger.LogWarning($"No MeshFilter found on selected Item [{item.objectItem.name}]");
+                    }
+                    else Logger.LogWarning("Selected object is not an Item");
+
+                }
+
+                if (runImport)
+                {
+                    if (path.EndsWith(".obj") || path.EndsWith(".OBJ"))
+                    {
+                        mesh = meshFromObj(path);
+                        Logger.LogInfo($"Loaded mesh from file [{path}]");
+                        //Logger.LogInfo($"Vertex count obj: {mesh.vertexCount}");
+                        //Logger.LogInfo($"Triangle count obj: {mesh.triangles.Length}");
+                        if (mesh == null)
+                            return;
+                        foreach (var i in selectItems)
+                        {
+                            remeshObject(i, mesh);
+                            OCIItem item = (OCIItem)i;
+                            Logger.LogInfo($"Mesh applied to object [{item.objectItem.name}]");
+                            i.treeNodeObject.textName = path.Substring(path.LastIndexOf("/")).Remove(0,1);
+                        }
+                    }
+                    else
+                    {
+                        Logger.LogWarning($"File [{path}] is not an OBJ file");
+
+                    }
+                    
+                } 
+            }
+        }
+        private Mesh meshFromObj(string path)
+        {
+            Mesh mesh = new Mesh();
+            string[] lines = File.ReadAllLines(path);
+            int vertexCount = 0;
+
+            foreach (string line in lines)
+            {
+                if (line.StartsWith("f "))
+                {
+                    vertexCount++;
+                }
+            }
+            vertexCount = vertexCount * 3;
+
+            mesh = new ObjImporter().ImportFile(path, (vertexCount > 65000));
+            if (mesh == null)
+                Logger.LogError("Mesh could not be loaded.");
+            return mesh;
+        }
+
+        public static void remeshObject(ObjectCtrlInfo oci, Mesh mesh)
+        {
+            OCIItem item = (OCIItem)oci;
+            item.objectItem.GetComponentInChildren<MeshFilter>().mesh = mesh;
+            remeshedObjects.Add(oci);
+        }
+          
+        void OnGUI()
+        {
+            if (uiActive)
+            {
+                windowRect = GUI.Window(345, windowRect, WindowFunction, "Obj Import");
+                KKAPI.Utilities.IMGUIUtils.EatInputInRect(windowRect);
+            }
+        }
+        private void WindowFunction(int WindowID)
+        {
+            path = GUI.TextField(new Rect(10, 20, 180, 20), path);
+            if (GUI.Button(new Rect(10, 50 , 180, 20), "Import OBJ"))
+            {
+                LoadMesh();
+            }
+            GUI.DragWindow();
+        }
+    }
+}
---
+++ Neuer Ordner/KKS_ObjImport/Objimport.SceneController.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using BepInEx;
+using BepInEx.Logging;
+using KKAPI;
+using Studio;
+using KKAPI.Studio.SaveLoad;
+using KKAPI.Utilities;
+using ExtensibleSaveFormat;
+using MessagePack;
+
+namespace ObjImport
+{
+    class SceneController: SceneCustomFunctionController
+    {
+        protected override void OnSceneSave()
+        {
+            PluginData data = new PluginData();
+
+            List<int> IDs = new List<int>();
+            List<Mesh> meshes = new List<Mesh>();
+
+            Dictionary<int, ObjectCtrlInfo> idObjectPairs = Studio.Studio.Instance.dicObjectCtrl;
+            foreach (int id in idObjectPairs.Keys)
+            {
+                if (ObjImport.remeshedObjects.Contains(idObjectPairs[id]))
+                {
+                    ObjImport.Logger.LogDebug($"Mesh saved for ID: [{id}] | {idObjectPairs[id].guideObject.name}");
+                    OCIItem item = (OCIItem)idObjectPairs[id];
+                    IDs.Add(id);
+                    meshes.Add(item.objectItem.GetComponentInChildren<MeshFilter>().mesh);
+                }
+            }
+            if (meshes.Count > 0)
+            {
+                data.data.Add("meshes", SimpleMeshSerializer.Serialize(meshes));
+                data.data.Add("ids", MessagePackSerializer.Serialize(IDs));
+            }
+            else
+            {
+                data.data.Add("meshes", null);
+                data.data.Add("ids", null);
+            }
+
+            SetExtendedData(data);
+        }
+
+        protected override void OnSceneLoad(SceneOperationKind operation, ReadOnlyDictionary<int, ObjectCtrlInfo> loadedItems)
+        {
+            var data = GetExtendedData();
+
+            if (operation == SceneOperationKind.Clear || operation == SceneOperationKind.Load)
+            {
+                ObjImport.remeshedObjects.Clear();
+            }
+
+            if (data == null) return;
+            if (operation == SceneOperationKind.Clear) return;
+
+            List<int> IDs = new List<int>();
+            List<Mesh> meshes = new List<Mesh>();
+
+            if (data.data.TryGetValue("meshes", out var meshesSerialized) && meshesSerialized != null)
+            {
+                meshes = (List<Mesh>)SimpleMeshSerializer.Deserialize((byte[])meshesSerialized);
+            }
+            if (data.data.TryGetValue("ids", out var ids) && ids != null)
+            {
+                IDs = MessagePackSerializer.Deserialize<List<int>>((byte[])ids);
+            }
+            if (IDs.Count > 0)
+                for (int x = 0; x < IDs.Count; x++)
+                {
+                    OCIItem item = (OCIItem)loadedItems[IDs[x]];
+                    Mesh mesh = meshes[x];
+                    ObjImport.remeshObject(item, mesh);
+                    ObjImport.Logger.LogDebug($"Mesh loaded for (old) ID [{IDs[x]}]:  {item.objectItem.name}");
+                }
+        }
+        protected override void OnObjectDeleted(ObjectCtrlInfo oci)
+        {
+            if(oci is OCIItem)
+            {
+                if (ObjImport.remeshedObjects.Contains(oci))
+                {
+                    ObjImport.remeshedObjects.Remove(oci);
+                }
+            }
+        }
+        protected override void OnObjectsCopied(ReadOnlyDictionary<Int32, ObjectCtrlInfo> copiedItems)
+        {
+            Dictionary<int, ObjectCtrlInfo> sceneObjects = Studio.Studio.Instance.dicObjectCtrl;
+            foreach (int id in copiedItems.Keys)
+            {
+                if (copiedItems[id] is OCIItem)
+                {
+                    OCIItem newItem = (OCIItem)copiedItems[id];
+                    OCIItem oldItem = (OCIItem)sceneObjects[id];
+                    if (ObjImport.remeshedObjects.Contains(sceneObjects[id]))
+                    {
+                        ObjImport.remeshObject(copiedItems[id], oldItem.objectItem.GetComponentInChildren<MeshFilter>().mesh);
+                        newItem.treeNodeObject.textName = oldItem.treeNodeObject.textName;
+                        ObjImport.Logger.LogDebug($"Mesh copied from {oldItem.objectItem.name} to {newItem.objectItem.name}");
+                    }
+                }
+            }
+        }
+    }
+}
