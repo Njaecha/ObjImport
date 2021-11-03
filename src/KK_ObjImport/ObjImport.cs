94dab6323adc0da94ec92e57ec09b3333fc588ca
NiggoJaecha <andre.grabowich@gmail.com>
2021-11-04 00:24:43 +0100

    Update 1.2.2
    added axis based mirroring.

--- src/KK_ObjImport/ObjImport.cs
+++ src/KK_ObjImport/ObjImport.cs
@@ -23,19 +23,23 @@ namespace ObjImport
 
         public const string PluginName = "KK_ObjImport";
         public const string GUID = "org.njaecha.plugins.objimport";
-        public const string Version = "1.2.1";
+        public const string Version = "1.2.2";
 
         internal new static ManualLogSource Logger;
 
         private bool uiActive = false;
         private ConfigEntry<KeyboardShortcut> hotkey;
         private ConfigEntry<string> defaultDir;
-        private Rect windowRect = new Rect(500, 40, 240, 140);
+        private Rect windowRect = new Rect(500, 40, 240, 170);
         private int scaleSelection = 0;
         private string[] scaleGridText = { "1", "0.5", "1.5", "2", "0.1", "0.01", "0.001", "0.0001" };
         private float[] scales = { 1f, 0.5f, 1.5f, 2f, 0.1f, 0.01f, 0.001f, 0.0001f };
         private bool displayHelp = false;
+        private bool displayAdvanced = false;
 
+        private bool flipX = false;
+        private bool flipY = false;
+        private bool flipZ = true;
 
         public static List<ObjectCtrlInfo> remeshedObjects = new List<ObjectCtrlInfo>();
 
@@ -133,7 +137,7 @@ namespace ObjImport
             {
                 Logger.LogError("Mesh too big, not supported in this version of Unity.");
             }
-            else if (scaleSelection != 0)
+            else
             {
                 Vector3[] baseVertices = mesh.vertices;
                 var vertices = new Vector3[baseVertices.Length];
@@ -143,13 +147,44 @@ namespace ObjImport
                     vertex.x = (float)(vertex.x * scales[scaleSelection]);
                     vertex.y = (float)(vertex.y * scales[scaleSelection]);
                     vertex.z = (float)(vertex.z * scales[scaleSelection]);
-
                     vertices[i] = vertex;
                 }
                 mesh.vertices = vertices;
-                mesh.RecalculateBounds();
+                flipCoordinates(mesh);
+
             }
             return mesh;
+        }
+        public void flipCoordinates(Mesh mesh)
+        {
+            Vector3[] baseVertices = mesh.vertices;
+            var vertices = new Vector3[baseVertices.Length];
+            for (var i = 0; i < vertices.Length; i++)
+            {
+                var vertex = baseVertices[i];
+                if (flipX) vertex.x = -vertex.x;
+                if (flipY) vertex.y = -vertex.y;
+                if (flipZ) vertex.z = -vertex.z;
+                vertices[i] = vertex;
+            }
+            mesh.vertices = vertices;
+            int flips = 0;
+            if (flipX) flips++;
+            if (flipY) flips++;
+            if (flipZ) flips++;
+            if (flips == 1 || flips == 3)   //inverse triangle order if a 1 or all axes got flipped
+            {
+                int[] baseTriangles = mesh.triangles;
+                int[] triangles = new int[baseTriangles.Length];
+                for (int x = 0; x < baseTriangles.Length; x += 3)
+                {
+                    triangles[x] = baseTriangles[x];
+                    triangles[x + 1] = baseTriangles[x + 2];
+                    triangles[x + 2] = baseTriangles[x + 1];
+                }
+                mesh.triangles = triangles;
+            }
+            mesh.RecalculateBounds();
         }
 
         public static void remeshObject(ObjectCtrlInfo oci, Mesh mesh)
@@ -183,21 +218,36 @@ namespace ObjImport
                 }
             }
             scaleSelection = GUI.SelectionGrid(new Rect(10, 50, 220, 40), scaleSelection, scaleGridText, 4);
-            if (GUI.Button(new Rect(10, 100, 180, 30), "Import OBJ"))
+            if (GUI.Button(new Rect(10, 100, 220, 30), "Import OBJ"))
             {
                 LoadMesh();
             }
-            if (GUI.Button(new Rect(190, 100, 40, 30), "Help"))
+            if (GUI.Button(new Rect(10, 135, 110, 20), "Help"))
             {
                 displayHelp = !displayHelp;
+                displayAdvanced = false;
+            }
+            if (GUI.Button(new Rect(120, 135, 110, 20), "Advanced"))
+            {
+                displayHelp = false;
+                displayAdvanced = !displayAdvanced;
             }
             if (displayHelp)
             {
-                windowRect.height = 270;
+                windowRect.height = 285;
                 string helpText = "1. Select a studio item you want to replace.\n2. Enter an .obj file via it's filepath or the open-file-dialogue [...].\n3. Select the import-scaling-modifier in the selectiongrid above.\n4. Press 'Import'. If nothing happens, check the console for errors.";
-                GUI.Label(new Rect(10, 135, 220, 130), helpText);
+                GUI.Label(new Rect(10, 155, 220, 130), helpText);
             }
-            else windowRect.height = 140;
+            if (displayAdvanced)
+            {
+                windowRect.height = 245;
+                GUI.Label(new Rect(10, 155, 220, 20), "Mirror along axis:");
+                flipX = GUI.Toggle(new Rect(10, 180, 70, 20), flipX, " X-Axis");
+                flipY = GUI.Toggle(new Rect(10, 200, 70, 20), flipY, " Y-Axis");
+                flipZ = GUI.Toggle(new Rect(10, 220, 70, 20), flipZ, " Z-Axis");
+                GUI.Label(new Rect(80, 180, 150, 60), "With default export settings in Blender, Z should be mirrored!");
+            }
+            if (!displayHelp && !displayAdvanced) windowRect.height = 165;
             GUI.DragWindow();
         }
     }
